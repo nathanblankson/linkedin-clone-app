cf4a53f15ce277e7a47c340e0b4108078874eade
Nathan Blankson <nathan@sourcebreaker.com>
2023-01-02 14:57:55 +0000

    Re-structure some css + adding some classes for mobile view

---
+++ src/app/core/components/layout/mobile-tabs/mobile-tabs.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-tabs',
+  templateUrl: './mobile-tabs.component.html',
+  styleUrls: ['./mobile-tabs.component.scss'],
+})
+export class MobileTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
